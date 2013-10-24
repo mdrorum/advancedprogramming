2b2fae3d27f3d23d80c9406f4715bd6dac3fd94a
Carlos León <clnznr@gmail.com>
2013-10-24 19:26:10 +0200

    more bots...

--- bots/bots.h
+++ bots/bots.h
@@ -16,18 +16,30 @@ public:
 
     typedef unsigned short status;
 
-    Bot () {};
+    Bot () : mBaseAttack(2), mBaseDefense(1), mExperience(0), mEnergy(100) {};
     virtual ~Bot () {};
 
     status GetEnergy() const {};
 
+    inline status GetBaseAttack() const {
+        return mBaseDefense;
+    };
+
     status GetAttack() const {};
+
+    inline status GetBaseDefense() const {
+        return mBaseDefense;
+    };
 
     status GetDefense() const {};
 
     status GetExperience() const {};
 
 private:
+    status mBaseAttack;
+    status mBaseDefense;
+    status mExperience;
+    status mEnergy;
     
 };
 
@@ -41,18 +53,20 @@ public:
     typedef unsigned int field_size ;
 
     Bots (field_size width, field_size height) : mWidth(width), mHeight(height)
-    {};
+    {
+        //mMap = new Bot [mWidth][mHeight];
+    };
 
     void Generate(size_t number_teams, size_t bots_per_team) {
 
-        random_device rd;
+        std::random_device rd;
         std::mt19937 gen(rd());
         std::mt19937 gen2(rd());
-        uniform_int_distribution <usigned int> random_width(0, mWidth - 1);
-        uniform_int_distribution <usigned int> random_height(0, mHeight - 1);
+        std::uniform_int_distribution <field_size> random_width(0, mWidth - 1);
+        std::uniform_int_distribution <field_size> random_height(0, mHeight - 1);
 
         if(number_teams * bots_per_team > mWidth * mHeight) {
-            throw something;
+            //throw something;
         }
 
         bot_id id = 0;
@@ -89,15 +103,24 @@ public:
         std::cout << time << std::endl;
     };
 
-    void CreateBot(field_size x, field_size y, team_id team, bot_id bot) {}
+    void CreateBot(field_size x, field_size y, team_id team, bot_id id) {
+
+        // move!
+        Bot bot;
+        mMap[team][std::pair(x, y)] = bot(id);
+    }
 
     // move!
     const std::map<bot_id, Bot> & GetBots(team_id team) const {};
 
 private:
+
+    typedef std::map<std::pair<field_size, field_size>, Bot> team;
+
     field_size mWidth;
     field_size mHeight;
-    Bot mMap[][];
+    std::map <team_id, team> mMap;
+
 };
 
 

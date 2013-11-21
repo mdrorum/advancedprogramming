9b5374211777299ad058adaa53a0e576d9197c83
Carlos León <clnznr@gmail.com>
2013-11-21 12:06:44 +0100

    templates

--- lectures/lecture8/README.md
+++ lectures/lecture8/README.md
@@ -1,10 +1,13 @@
 # Lecture 8: Templates
 
-- Templates
+- Templates: 
+    - generic programming: http://en.wikipedia.org/wiki/Generic_programming
     - Advanced templates
-    - Template metaprogramming
-    - Template specialization
-    - Traits
-
+        - Template metaprogramming: http://en.wikibooks.org/wiki/C%2B%2B_Programming/Templates/Template_Meta-Programming
+        - Template specialization
+        - Traits
 
 # Exercises
+
+- your classes for managing the intelligence or robots must be parameterized with traits:
+   -  
---
+++ lectures/lecture8/templates.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+// templates lets us use generic programming in C++
+// http://en.wikipedia.org/wiki/Generic_programming
+template <typename N>
+N add(N a, N b) {
+    return a + b;
+}
+
+template <int n>
+struct Const {
+    enum { value = n };
+};
+
+////////////////////////////////////////////////////////
+// template specialization and metaprogramming
+template <unsigned n>
+struct factorial
+{
+  enum { value = n * factorial<n-1>::value };
+};
+template <>          // | look at this syntax!
+struct factorial<0>  // |
+{
+  enum { value = 1 };
+};
+////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////
+// boost have traits
+// http://www.boost.org/doc/libs/1_55_0/libs/type_traits/doc/html/index.html
+
+template<typename T>
+struct can_be_incremented {
+    static const bool value = false; // static-const-things can also be used
+};
+
+template<>
+struct can_be_incremented <int> {
+    static const bool value = true;
+};
+
+template<typename A, bool>
+struct increment {
+};
+
+template<typename A>
+struct increment <A, true> {
+    constexpr static A increment_number(A a) {
+        return a++;
+    }
+};
+
+template<typename A>
+struct increment_type {
+    constexpr static A increment_number(A a) {
+        return increment<A, can_be_incremented<A>::value>::increment_number(a);
+    }
+};
+
+////////////////////////////////////////////////////////
+
+
+int main()
+{
+    
+#define ALTERNATIVE 5
+#ifdef ALTERNATIVE
+    std::cout << "yeah, compile-time programming existed before C++" << std::endl;
+#else
+    std::cout << "but not everything will be COMPILED" << std::endl;
+#endif
+
+#ifndef SOME_UNDEFINED_PREPROCESSOR_SYMBOL
+    std::cout << "prints!" << std::endl;
+#endif
+
+#if ALTERNATIVE > 5
+    std::cout << "comparisons are valid" << std::endl;
+#endif
+
+
+    // just to make it explicit...
+    std::cout << "template adding with float: " << add((int)3, (int)5) << std::endl;
+
+    std::cout << "template adding with int: " << add((float)3.7, (float)15.0) << std::endl;
+
+    int a = Const<9>::value;
+    std::cout << "const: " << a << std::endl;
+
+    int b = factorial<5>::value;
+    std::cout << "factorial: " << b << std::endl;
+
+    // traits
+
+    int j = 9;
+    std::cout << "incremented: " << increment_type<decltype(j)>::increment_number(j) << std::endl;
+
+    // non-compiling template
+    //std::cout << increment_type<char>::increment_number('a') << std::endl;
+
+    return 0;
+}
+
